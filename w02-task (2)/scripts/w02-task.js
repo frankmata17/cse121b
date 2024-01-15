8c6283cee00b33c02a399eb7393132fe84a7046f
frankmata17 <frank.mata17@gmail.com>
2024-01-15 10:55:37 -0700

    w02 commit

---
+++ w02-task (2)/scripts/w02-task.js
@@ -0,0 +1,39 @@
+/* W02-Task - Profile Home Page */
+
+/* Step 1 - Setup type tasks - no code required */
+
+/* Step 2 - Variables */
+let fullName = 'Francisco Mata';
+let currentYear = new Date().getFullYear();
+const profilePicture = 'images/Me.jpg';
+
+
+/* Step 3 - Element Variables */
+const nameElement = document.getElementById('name');
+const yearElement = document.querySelector('#year');
+const foodElement = document.getElementById('favoriteFoods');
+
+const pictureElement = document.querySelector('picture');
+
+const imageElement = document.createElement('img');
+imageElement.setAttribute('src', 'images/Me.jpg');
+imageElement.setAttribute('alt', 'Description of image');
+
+pictureElement.appendChild(imageElement);
+
+/* Step 4 - Adding Content */
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
+yearElement.textContent = currentYear;
+
+let favoriteFoods = ['Pozole', 'Pizza', 'Tamales', 'Gelato'];
+foodElement.innerHTML = `${favoriteFoods}`;
+favoriteFoods.push('Pasta');
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.shift();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+favoriteFoods.pop();
+foodElement.innerHTML += `<br>${favoriteFoods}`;
+
+
+
+
